2da82e9053a0d549e8d30282da66ec463d1736e8
LucHighwalker <luc.highwalker@gmail.com>
2019-01-10 12:23:40 -0800

    - Improved dashboard.

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../services/user.service';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'wtf-dashboard',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  public curToken: string;
+  public curUser: any;
+
+  constructor(private user: UserService, private cookie: CookieService) { }
 
   ngOnInit() {
+    this.user.userUpdate.subscribe(user => {
+      this.curUser = user;
+    });
+    this.curToken = this.cookie.get('wtf-user-token');
+    this.user.updateUser(this.curToken);
   }
 
 }
